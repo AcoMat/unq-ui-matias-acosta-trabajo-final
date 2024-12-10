d826d05a252359b9b2c01928e10cb883a5fdda3a
Acosta Matias <aaco.maty@gmail.com>
2024-12-10 17:04:00 -0300

    Implemented game state management with context

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useContext } from 'react'
 import './App.css'
+import { GameStateContext } from './context/GameStateContext'
+import Board from './components/board/Board';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { GameStages, stage, nextStage, reset } = useContext(GameStateContext);
 
   return (
-    <>
+    stage === GameStages[0] ?
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <h1>MemoTest</h1>
+        </div>
+        <div>
+          <button onClick={nextStage}>Jugar</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      :
+      stage === GameStages[1] ?
+        <div>
+          <h2>Elegí una dificultad</h2>
+          <div>
+            <button onClick={nextStage}>Fácil</button>
+            <button onClick={nextStage}>Medio</button>
+            <button onClick={nextStage}>Difícil</button>
+          </div>
+        </div>
+        :
+        stage === GameStages[2] ?
+          <div>
+            <h2>Jugando</h2>
+            <Board height={4} width={4}/>
+            <div>
+              <button onClick={nextStage}>Terminar</button>
+            </div>
+          </div>
+          :
+          <div>
+            <h2>Game Over</h2>
+            <div>
+              <button onClick={reset}>Volver a jugar</button>
+            </div>
+          </div>
   )
 }
 
---
+++ src/context/GameStateContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState } from "react";
+
+export const GameStateContext = createContext();
+
+export const GameStateProvider = ({ children }) => {
+    const GameStages = ["MENU", "CHOOSINGDIF", "PLAYING", "GAME_OVER"];
+    const [stage, setStage] = useState(GameStages[0]);
+
+    const nextStage = () => {
+        console.log((GameStages.indexOf(stage) + 1));
+        setStage(GameStages[(GameStages.indexOf(stage) + 1) % GameStages.length]);
+    };
+
+    const reset = () => {
+        setStage(GameStages[0]);
+    };
+
+    return (
+        <GameStateContext.Provider value={{ GameStages, stage, nextStage, reset }}>
+            {children}
+        </GameStateContext.Provider>
+    );
+};
