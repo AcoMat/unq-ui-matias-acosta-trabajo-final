efd3d22e189fef827c1d8a7c6177c60e0a9b60e6
Acosta Matias <aaco.maty@gmail.com>
2024-12-10 20:07:10 -0300

    Flip animation working

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,15 +1,15 @@
 import './Card.css';
 import reverso from '../../assets/cards/reverso.jpg';
+import { useState } from 'react';
 function Card({ image }) {
+    const [active, setActive] = useState(false);
     return (
-        <div className='card'>
-            <div className='card-inner'>
-                <div className='card-front'>
-                    <img src={image} alt="card" className='card-image' />
-                </div>
-                <div className='card-back'>
-                    <img src={reverso} alt="card" className='card-image' />
-                </div>
+        <div className={`flip-card ${active ? '' : 'inactive'}`} onClick={() => setActive(!active)}>
+            <div className='flip-card-front'>
+                <img src={image} alt="card" className='card-image' />
+            </div>
+            <div className='flip-card-back'>
+                <img src={reverso} alt="card" className='card-image' />
             </div>
         </div>
     );
